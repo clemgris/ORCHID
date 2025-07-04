24c1490bd417930999a3b0051395bd509a2d96a4
clemgris <clemence.grislain@eleves.enpc.fr>
2025-07-04 15:54:55 +0200

    Save image tool assumed normalised inputs

--- flowdiffusion/evaluate_policy/utils/vis.py
+++ flowdiffusion/evaluate_policy/utils/vis.py
@@ -1,3 +1,4 @@
+import numpy as np
 import PIL.Image as Image
 
 
@@ -8,8 +9,8 @@ def save_gif(obs_list, save_path, duration=0.2):
         # Convert from CHW torch tensor to HWC numpy array
         img_np = img.detach().cpu().permute(1, 2, 0).numpy()
 
-        # Normalize from [-1, 1] to [0, 255]
-        # img_np = ((img_np + 1) / 2 * 255).clip(0, 255).astype(np.uint8)
+        #  Normalize from [-1, 1] to [0, 255]
+        img_np = ((img_np + 1) / 2 * 255).clip(0, 255).astype(np.uint8)
 
         # Convert to PIL Image in RGB mode
         frames.append(Image.fromarray(img_np).convert("RGB"))
