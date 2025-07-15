6476c6a3b97e4da5c803a0cb4c6b509aee5c7d0c
clemgris <clemence.grislain@eleves.enpc.fr>
2025-07-15 15:42:31 +0200

    Add params to saving gif function

--- flowdiffusion/evaluate_policy/utils/vis.py
+++ flowdiffusion/evaluate_policy/utils/vis.py
@@ -2,7 +2,7 @@ import numpy as np
 import PIL.Image as Image
 
 
-def save_gif(obs_list, save_path, duration=0.2):
+def save_gif(obs_list, save_path, duration=0.2, norm=True):
     frames = []
 
     for img in obs_list:
@@ -10,7 +10,9 @@ def save_gif(obs_list, save_path, duration=0.2):
         img_np = img.detach().cpu().permute(1, 2, 0).numpy()
 
         #  Normalize from [-1, 1] to [0, 255]
-        img_np = ((img_np + 1) / 2 * 255).clip(0, 255).astype(np.uint8)
+        if norm:
+            img_np = (img_np + 1) / 2
+        img_np = (img_np * 255).clip(0, 255).astype(np.uint8)
 
         # Convert to PIL Image in RGB mode
         frames.append(Image.fromarray(img_np).convert("RGB"))
