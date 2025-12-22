08c1b1c7d8e26d90556d832214fc4f5e122a928f
clemgris <clemence.grislain@eleves.enpc.fr>
2025-12-22 20:00:56 +0100

    Script to debug expert

--- toy_env_pybullet/test.py
+++ toy_env_pybullet/test.py
@@ -52,65 +52,68 @@ stats = {
 
 neutral_pos = torch.tensor([0.5, 0.0, 0.4])
 
-task_id = 0
+task_id = 3
 
-for episode in range(1):
-    if task_id == 3:
-        # Chose bottom and up cube
-        (up_cube, bottom_cube) = random.sample(["A", "B", "C"], 2)
-        expert_pol = lambda x, y: env.expert_stack(  # noqa: E731
-            x, y, up_cube=up_cube, bottom_cube=bottom_cube
-        )
-        reward_fn = lambda x, y: env.compute_reward_stack(  # noqa: E731
-            x, y, up_cube=up_cube, bottom_cube=bottom_cube
-        )
-    else:
-        expert_pol = env.get_expert_policy(task_id=task_id)
-        reward_fn = env.get_reward_function(task_id)
-    frames = []
-    success = False
-    imgs = []
+if task_id == 3:
+    # Chose bottom and up cube
+    up_cube, bottom_cube = random.sample(["A", "B", "C"], k=2)
 
-    # Reset environment
-    obs = env.reset(task_id=task_id)
-    init_obs = obs.copy()
+    print(f"Stacking cube {up_cube} on cube {bottom_cube}")
 
-    count_done = 0
-
-    for step in range(64):
-        # Check if task is complete
-        if count_done > 5:
-            # Return to neutral position
-            actions = env._go_to(obs["state"], neutral_pos, gripper_cmd=1)
-            success = True
-        else:
-            # Execute expert policy
-            actions = expert_pol(init_obs, obs)
-
-        # Store frame data
-        frame = {
-            "actions": actions.cpu().numpy(),
-            "states": obs["state"].cpu().numpy(),
-            "rgb_static": obs["pixels"][0].permute(1, 2, 0).cpu().numpy(),
-        }
-        frames.append(frame)
-
-        # Step environment
-        obs, _, dones, info = env.step(actions)
-        img = obs["pixels"][0].permute(1, 2, 0).cpu().numpy()
-        pil_img = Image.fromarray((img * 255).astype(np.uint8))
-        imgs.append(pil_img)
-
-        # Check reward
-        rewards = reward_fn(init_obs, obs)
-        if rewards > 0.5:
-            count_done += 1
-
-    imgs[0].save(
-        os.path.join("debug_toy_env", TASK_NAMES[task_id], "example.gif"),
-        format="GIF",
-        append_images=imgs[1:],
-        save_all=True,
-        duration=100,  # ms per frame
-        loop=0,
+    expert_pol = lambda x, y: env.expert_stack(  # noqa: E731
+        x, y, up_cube=up_cube, bottom_cube=bottom_cube
     )
+    reward_fn = lambda x, y: env.compute_reward_stack(  # noqa: E731
+        x, y, up_cube=up_cube, bottom_cube=bottom_cube
+    )
+else:
+    expert_pol = env.get_expert_policy(task_id=task_id)
+    reward_fn = env.get_reward_function(task_id)
+frames = []
+success = False
+imgs = []
+
+# Reset environment
+obs = env.reset(task_id=task_id)
+init_obs = obs.copy()
+
+count_done = 0
+
+for step in range(64):
+    # Check if task is complete
+    if (count_done > 5) and step > 50:
+        # Return to neutral position
+        actions = env._go_to(obs["state"], neutral_pos, gripper_cmd=1)
+        success = True
+    else:
+        # Execute expert policy
+        actions = expert_pol(init_obs, obs)
+
+    # Store frame data
+    frame = {
+        "actions": actions.cpu().numpy(),
+        "states": obs["state"].cpu().numpy(),
+        "rgb_static": obs["pixels"][0].permute(1, 2, 0).cpu().numpy(),
+    }
+    frames.append(frame)
+
+    # Step environment
+    obs, _, dones, info = env.step(actions)
+    img = obs["pixels"][0].permute(1, 2, 0).cpu().numpy()
+    pil_img = Image.fromarray((img * 255).astype(np.uint8))
+    imgs.append(pil_img)
+
+    # Check reward
+    rewards = reward_fn(init_obs, obs)
+    if rewards > 0.5:
+        count_done += 1
+
+os.makedirs("debug_toy_env", exist_ok=True)
+imgs[0].save(
+    os.path.join("debug_toy_env", f"example_{TASK_NAMES[task_id]}.gif"),
+    format="GIF",
+    append_images=imgs[1:],
+    save_all=True,
+    duration=100,  # ms per frame
+    loop=0,
+)
